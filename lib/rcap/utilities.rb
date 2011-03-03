f10e2b0a069586f9d1e0dc76c27a808daf5ce1b9
Farrel Lifson <farrel.lifson@aimred.com>
2011-03-03 23:05:57 +0200

    Time zone formatting

--- lib/rcap/utilities.rb
+++ lib/rcap/utilities.rb
@@ -30,10 +30,14 @@ end
 
 class Time # :nodoc:
   RCAP_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
-  RCAP_ZONE_FORMAT = "%+02i:00"
+  RCAP_ZONE_FORMAT = "%+03i:00"
 
   def to_s_for_cap
     self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , self.utc_hours_offset ) 
+  end
+
+  def utc_hours_offset
+    self.utc_offset/3600
   end
 end
 
