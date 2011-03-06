ee5bb8ed63d4bec39008979408f3502f262be4dc
Farrel Lifson <farrel.lifson@aimred.com>
2011-03-06 10:46:32 +0200

    Documentation cleanup

--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -6,11 +6,11 @@ describe( RCAP::Resource ) do
       @resource = RCAP::Resource.new
     end
 
-    it( 'should have no mime_type' ){ @resource.mime_type.should( be_nil )}    
-    it( 'should have no size' ){ @resource.size.should( be_nil )}         
-    it( 'should have no uri' ){ @resource.uri.should( be_nil )}          
-    it( 'should have no deref_uri' ){ @resource.deref_uri.should( be_nil )}    
-    it( 'should have no digest' ){ @resource.digest.should( be_nil )}       
+    it( 'should have no mime_type' ){     @resource.mime_type.should( be_nil )}    
+    it( 'should have no size' ){          @resource.size.should( be_nil )}         
+    it( 'should have no uri' ){           @resource.uri.should( be_nil )}          
+    it( 'should have no deref_uri' ){     @resource.deref_uri.should( be_nil )}    
+    it( 'should have no digest' ){        @resource.digest.should( be_nil )}       
     it( 'should have no resource_desc' ){ @resource.resource_desc.should( be_nil )}
 
     context( 'from XML' ) do
