40406ed12a7ab3852e675661766a7d7cdddaf195
Farrel Lifson <farrel.lifson@aimred.com>
2011-03-13 20:48:54 +0200

    Can initialise CAP 1.1 alert from RCAP::Alert factory.

---
+++ lib/rcap/alert.rb
@@ -0,0 +1,36 @@
+module RCAP
+  module Alert
+
+    XMLNS_KEY            = "xmlns" 
+    YAML_CAP_VERSION_KEY = "CAP Version"
+    JSON_CAP_VERSION_KEY = "cap_version"
+
+    def self.from_xml( xml, namespace_key = XMLNS_KEY )
+      xml_document = REXML::Document.new( xml )
+
+      case xml_document.root.namespaces[ namespace_key ]
+      when CAP_1_1::Alert::XMLNS 
+        CAP_1_1::Alert.from_xml_document( xml_document )
+      end
+
+    end
+
+    def self.from_yaml( yaml )
+      yaml_data = YAML.load( yaml )
+
+      case yaml_data[ YAML_CAP_VERSION_KEY ]
+      when CAP_1_1::Alert::CAP_VERSION
+        CAP_1_1::Alert.from_yaml_data( yaml_data )
+      end
+    end
+
+    def self.from_json( json_string )
+      json_hash = JSON.parse( json_string )
+      
+      case json_hash[ JSON_CAP_VERSION_KEY ]
+      when CAP_1_1::Alert::CAP_VERSION
+        CAP_1_1::Alert.from_h( json_hash )
+      end
+    end
+  end
+end
---
+++ spec/alert_spec.rb
@@ -0,0 +1,83 @@
+describe( RCAP::Alert ) do
+  describe( 'initialising' ) do
+
+    context( 'a CAP 1.1 alert' ) do
+      before( :each ) do
+        @original_alert = RCAP::CAP_1_1::Alert.new( :sender => 'Sender',
+                                                    :status => RCAP::CAP_1_1::Alert::STATUS_TEST,
+                                                    :scope => RCAP::CAP_1_1::Alert::SCOPE_PUBLIC,
+                                                    :source => 'Source',
+                                                    :restriction => 'No Restriction',
+                                                    :addresses => [ 'Address 1', 'Address 2'],
+                                                    :code => 'Code',
+                                                    :note => 'Note',
+                                                    :references => [ RCAP::CAP_1_1::Alert.new( :sender => 'Sender1' ).to_reference, 
+                                                                     RCAP::CAP_1_1::Alert.new( :sender => 'Sender2' ).to_reference ],
+                                                    :incidents => [ 'Incident1', 'Incident2' ],
+                                                    :infos => [ RCAP::CAP_1_1::Info.new, RCAP::CAP_1_1::Info.new ])
+      end
+
+
+
+      shared_examples_for( 'it has parsed a CAP 1.1 alert correctly' ) do
+        it( 'should use the correct CAP Version' ){ @alert.class.should       == RCAP::CAP_1_1::Alert }
+        it( 'should parse identifier correctly' ) { @alert.identifier.should  == @original_alert.identifier }
+        it( 'should parse sender correctly' )     { @alert.sender.should      == @original_alert.sender }
+        it( 'should parse sent correctly' )       { @alert.sent.should( be_close( @original_alert.sent, Rational( 1, 86400 )))}
+        it( 'should parse status correctly' )     { @alert.status.should      == @original_alert.status }
+        it( 'should parse msg_type correctly' )   { @alert.msg_type.should    == @original_alert.msg_type }
+        it( 'should parse source correctly' )     { @alert.source.should      == @original_alert.source }
+        it( 'should parse scope correctly' )      { @alert.scope.should       == @original_alert.scope }
+        it( 'should parse restriction correctly' ){ @alert.restriction.should == @original_alert.restriction }
+        it( 'should parse addresses correctly' )  { @alert.addresses.should   == @original_alert.addresses }
+        it( 'should parse code correctly' )       { @alert.code.should        == @original_alert.code }
+        it( 'should parse note correctly' )       { @alert.note.should        == @original_alert.note }
+        it( 'should parse references correctly' ) { @alert.references.should  == @original_alert.references }
+        it( 'should parse incidents correctly' )  { @alert.incidents.should   == @original_alert.incidents }
+        it( 'should parse infos correctly' ) do 
+          @alert.infos.size.should == @original_alert.infos.size 
+          @alert.infos.each{ |info| info.class.should == RCAP::CAP_1_1::Info }
+        end
+      end
+
+      context( 'from XML' ) do
+        before( :each ) do
+          @alert = RCAP::Alert.from_xml( @original_alert.to_xml )
+        end
+
+        it_should_behave_like( 'it has parsed a CAP 1.1 alert correctly' )
+      end
+
+      context( 'from YAML' ) do
+        before( :each ) do
+          @alert = RCAP::Alert.from_yaml( @original_alert.to_yaml )
+        end
+
+        it_should_behave_like( 'it has parsed a CAP 1.1 alert correctly' )
+      end
+
+      context( 'from JSON' ) do
+        before( :each ) do
+          @alert = RCAP::Alert.from_json( @original_alert.to_json )
+        end
+
+        it_should_behave_like( 'it has parsed a CAP 1.1 alert correctly' )
+      end
+    end
+
+    context( 'a CAP 1.2 alert' ) do
+
+      shared_examples_for 'it has parsed a CAP 1.2 alert correctly' do
+      end
+
+      context( 'from XML' ) do
+      end
+
+      context( 'from YAML' ) do
+      end
+
+      context( 'from JSON' ) do
+      end
+    end
+  end
+end
