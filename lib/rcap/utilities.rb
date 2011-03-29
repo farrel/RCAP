adb2f06ee1a831fbc0449de42e546ee4f9dcc400
Earle Clubb <eclubb@valcom.com>
2011-03-28 20:13:04 -0400

    Fix time format when time zone is UTC.
    Per http://docs.oasis-open.org/emergency/cap/v1.2/CAP-v1.2-os.html
    Time zone separator, X, should be '-' when time zone is UTC.

--- lib/rcap/utilities.rb
+++ lib/rcap/utilities.rb
@@ -33,7 +33,8 @@ class Time # :nodoc:
   RCAP_ZONE_FORMAT = "%+03i:00"
 
   def to_s_for_cap
-    self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , self.utc_hours_offset )
+    t = self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , self.utc_hours_offset )
+    t.sub(/\+(00:\d\d)$/, '-\1')
   end
 
   def utc_hours_offset
