bfb0933390ae185f93535a16f769e390e49d42e3
Farrel Lifson <farrel.lifson@aimred.com>
2011-04-04 20:53:44 +0200

    Allow polygon text to be empty

--- lib/rcap/cap_1_1/polygon.rb
+++ lib/rcap/cap_1_1/polygon.rb
@@ -54,7 +54,7 @@ module RCAP
       end
 
       def self.from_xml_element( polygon_xml_element ) # :nodoc:
-        if !polygon_xml_element.text.nil?
+        if !polygon_xml_element.text.nil? && !polygon_xml_element.text.empty?
           coordinates = self.parse_polygon_string( polygon_xml_element.text )
           points = coordinates.map{ |lattitude, longitude| Point.new( :lattitude => lattitude, :longitude => longitude )}[0..-2]
           polygon = self.new( :points => points )
