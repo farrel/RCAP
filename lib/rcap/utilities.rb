64d0917bf4112178a96faa9b5821629e642f8f15
Farrel Lifson <farrel.lifson@aimred.com>
2011-07-28 18:39:07 +0200

    Documentation

--- lib/rcap/utilities.rb
+++ lib/rcap/utilities.rb
@@ -1,23 +1,64 @@
 ALLOWED_CHARACTERS = /[^\s&<]+/ # :nodoc:
 
-module RCAP # :nodoc:
+module RCAP  
+  # Returns a randomly generated UUID string
+  #
+  # @return [String] UUID string
   def self.generate_identifier
     UUIDTools::UUID.random_create.to_s
   end
 
+  # Returns the text of the first descendent that matches the given XPath
+  # expression.
+  #
+  # @param [REXML::Element] xml_element Element to start matching from.
+  # @param [String] xpath XPath expression
+  # @param [String] namepsace Namespace in which to do the matching
+  #
+  # @return [String,nil] Text content of element matching XPath query or nil 
   def self.xpath_text( xml_element, xpath, namespace )
     element = self.xpath_first( xml_element, xpath, namespace )
     element.text if element
   end
 
+  # Returns first descendent that matches the given XPath expression.
+  #
+  # @param [REXML::Element] xml_element Element to start matching from.
+  # @param [String] xpath XPath expression
+  # @param [String] namepsace Namespace in which to do the matching
+  #
+  # @return [REXML::Element,nil] Element matching XPath query or nil
   def self.xpath_first( xml_element, xpath, namespace )
     REXML::XPath.first( xml_element, xpath, { 'cap' => namespace })
   end
 
+  # Returns all descendents that match the given XPath expression.
+  #
+  # @param [REXML::Element] xml_element Element to start matching from.
+  # @param [String] xpath XPath expression
+  # @param [String] namepsace Namespace in which to do the matching
+  #
+  # @return [Array<REXML::Element>] Collection of elements matching XPath query 
   def self.xpath_match( xml_element, xpath, namespace )
     REXML::XPath.match( xml_element, xpath, { 'cap' => namespace })
   end
 
+  # Formats output for inspect
+  #
+  # @param [String] header Output header
+  # @param [String] inspect_string String to be output
+  # @return [String] Formatted output for inspect
+  #
+  # @example
+  #  RCAP.format_lines_for_inspect( 'Test', 'one\ntwo\nthree' ) 
+  #  # returns
+  #  # .-------.
+  #  # | Test  |
+  #  # |-------|
+  #  # | one   |
+  #  # | two   |
+  #  # | three |
+  #  # '-------'
   def self.format_lines_for_inspect( header, inspect_string )
     max_line_length = inspect_string.lines.map{ |line| line.chomp.length }.max
     "\n." + '-' * (max_line_length + 2) + ".\n"+
@@ -27,10 +68,21 @@ module RCAP # :nodoc:
       "'" + '-' * ( max_line_length + 2 ) + "'\n"
   end
 
+  # Converts an array of key value pairs into a hash, excluding any value that is nil or empty
+  #
+  # @param [Array<Array>] attribte_values An array of arrays of key/values.
+  # @return [Hash] Hash of attributes
+  #
+  # @example
+  #  RCAP.attribute_values_to_hash( ['a', 1], ['b' , []]) # => { 'a' => 1 }
   def self.attribute_values_to_hash( *attribute_values )
     Hash[ *attribute_values.reject{ |key, value| value.nil? || ( value.respond_to?( :'empty?' ) && value.empty? )}.flatten( 1 )]
   end
 
+  # Calls #to_s_for_cap on the object it it responds to that otherwise just calls #to_s
+  #
+  # @param [#to_s, #to_s_for_cap] 
+  # @return [String]
   def self.to_s_for_cap( object )
     if object
       if object.respond_to?( :to_s_for_cap )
@@ -41,6 +93,10 @@ module RCAP # :nodoc:
     end
   end
 
+  # If the parameter is a string the datetime is parsed out of it, otherwise returns nil.
+  #
+  # @param date_string Object to parse datetime out
+  # @return [String,nil]
   def self.parse_datetime( date_string )
     if date_string.is_a?( String )
       DateTime.parse( date_string )
