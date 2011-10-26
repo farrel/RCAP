f4319afe99bf5c5513dfbefdbf1b1f1f98604143
Farrel Lifson <farrel.lifson@aimred.com>
2011-10-26 22:01:52 +0200

    DateTime#to_s_for_cap

--- lib/extensions/date_time.rb
+++ lib/extensions/date_time.rb
@@ -1,4 +1,16 @@
 class DateTime # :nodoc:
+  RCAP_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+  RCAP_ZONE_FORMAT = "%+03i:00"
+
   alias inspect to_s
-  alias to_s_for_cap to_s
+
+  def to_s_for_cap
+    t = self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , utc_hours_offset )
+    t.sub(/\+(00:\d\d)$/, '-\1')
+  end
+
+  private
+  def utc_hours_offset
+    self.offset * 24
+  end
 end
--- lib/extensions/time.rb
+++ lib/extensions/time.rb
@@ -3,10 +3,11 @@ class Time # :nodoc:
   RCAP_ZONE_FORMAT = "%+03i:00"
 
   def to_s_for_cap
-    t = self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , self.utc_hours_offset )
+    t = self.strftime( RCAP_TIME_FORMAT ) + format( RCAP_ZONE_FORMAT , utc_hours_offset )
     t.sub(/\+(00:\d\d)$/, '-\1')
   end
 
+  private
   def utc_hours_offset
     self.utc_offset/3600
   end
