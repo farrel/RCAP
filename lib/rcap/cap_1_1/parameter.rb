b4b0cf78652dc2d09f2cf15cf7c3eed3524acd54
Farrel Lifson <farrel.lifson@aimred.com>
2011-12-20 01:12:16 +0200

    Paramete 1.1 and 1.2

--- lib/rcap/cap_1_1/parameter.rb
+++ lib/rcap/cap_1_1/parameter.rb
@@ -18,11 +18,15 @@ module RCAP
       NAME_XPATH  = "cap:#{ NAME_ELEMENT_NAME }"    
       VALUE_XPATH = "cap:#{ VALUE_ELEMENT_NAME }"   
 
+      # @param [Hash] attributes
+      # @option attributes [Symbol] :name Parameter name
+      # @option attributes [Symbol] :value Parameter value
       def initialize( attributes = {} )
         @name = attributes[ :name ]
         @value = attributes[ :value ]
       end
 
+      # @return [REXML::Element]
       def to_xml_element 
         xml_element = REXML::Element.new( self.class::XML_ELEMENT_NAME )
         xml_element.add_element( self.class::NAME_ELEMENT_NAME ).add_text( @name )
@@ -30,35 +34,47 @@ module RCAP
         xml_element
       end
 
+      # @return [String]
       def to_xml 
         self.to_xml_element.to_s
       end
 
+      # @return [String]
       def inspect 
         "#{ @name }: #{ @value }"
       end
 
       # Returns a string representation of the parameter of the form
       #  name: value
+      #
+      # @return [String]
       def to_s
         self.inspect
       end
 
+      # @param [REXML::Element] parameter_xml_element
+      # @return [Parameter] 
       def self.from_xml_element( parameter_xml_element ) # :nodoc:
         self.new( :name  => RCAP.xpath_text( parameter_xml_element, self::NAME_XPATH, Alert::XMLNS ),
                   :value => RCAP.xpath_text( parameter_xml_element, self::VALUE_XPATH, Alert::XMLNS ))
       end
 
       # Two parameters are equivalent if they have the same name and value.
+      #
+      # @param [Parameter] other
+      # @return [true, false]
       def ==( other )
         [ @name, @value ] == [ other.name, other.value ]
       end
 
+      # @return [Hash]
       def to_h 
         RCAP.attribute_values_to_hash(
           [ @name, @value ])
       end
 
+      # @param [Hash] hash
+      # @return [Parameter]
       def self.from_h( hash ) 
         key = hash.keys.first
         self.new( :name => key, :value => hash[ key ])
