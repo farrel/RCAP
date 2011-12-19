07206b830da175f57e5489bebb4f674c7ec27b32
Farrel Lifson <farrel.lifson@aimred.com>
2011-12-20 01:20:33 +0200

    Polygon 1.1 and 1.2

--- lib/rcap/cap_1_1/polygon.rb
+++ lib/rcap/cap_1_1/polygon.rb
@@ -6,7 +6,8 @@ module RCAP
     class Polygon
       include Validation
 
-      # Collection of Point objects.
+      # Collection of {Point} objects.
+      # @return [Array<Point>]
       attr_reader( :points )
 
       validates_collection_of( :points )
@@ -15,13 +16,16 @@ module RCAP
 
       XML_ELEMENT_NAME = 'polygon'                   
       XPATH            = "cap:#{ XML_ELEMENT_NAME }" 
-
+      
+      # @param [Hash] attributes
+      # @option attributes [Array<Point>] :points Collection of {Point} objects
       def initialize( attributes = {})
         @points = Array( attributes[ :points ])
       end
 
-      # Creates a new Point object and adds it to the points array. The
-      # poitn_attributes are passed as a parameter to Point.new.
+      # Creates a new Point object and adds it to the points array.
+      #
+      # @see Point#initialize
       def add_point( point_attributes = {})
         point = Point.new( point_attributes )
         @points << point
@@ -29,16 +33,18 @@ module RCAP
       end
 
       # Returns a string representation of the polygon of the form
-      #  points[0] points[1] points[2] ... 
+      #  points[0] points[1] points[2] ...
       # where each point is formatted with Point#to_s
       def to_s
         @points.join( ' ' )
       end
 
+      # @return [String]
       def inspect 
         "(#{ @points.map{|point| point.inspect}.join(', ')})"
       end
 
+      # @return [REXML::Element]
       def to_xml_element 
         xml_element = REXML::Element.new( XML_ELEMENT_NAME )
         xml_element.add_text( self.to_s )
@@ -46,14 +52,18 @@ module RCAP
       end
 
       # Two polygons are equivalent if their collection of points is equivalent.
+      # 
+      # @return [true,false]
       def ==( other )
         @points == other.points
       end
 
+      # @return [Array<Array(Numeric,Numeric)>]
       def self.parse_polygon_string( polygon_string ) 
         polygon_string.split( ' ' ).map{ |coordinate_string| coordinate_string.split( ',' ).map{|coordinate| coordinate.to_f }}
       end
 
+      # @return [Polygon]
       def self.from_xml_element( polygon_xml_element ) 
         if !polygon_xml_element.text.nil? && !polygon_xml_element.text.empty?
           coordinates = self.parse_polygon_string( polygon_xml_element.text )
@@ -65,20 +75,24 @@ module RCAP
       end
 
 
+      # @return [String]
       def to_yaml( options = {} ) 
         @points.map{ |point| [ point.lattitude, point.longitude ]}.to_yaml( options )
       end
 
+      # @return [Polygon]
       def self.from_yaml_data( polygon_yaml_data ) 
         self.new( :points => Array( polygon_yaml_data ).map{ |lattitude, longitude| Point.new( :lattitude => lattitude, :longitude => longitude )})
       end
 
       POINTS_KEY  = 'points' 
 
+      # @return [Hash]
       def to_h 
         { POINTS_KEY => @points.map{ |point| point.to_h }}
       end
 
+      # @return [Polygon]
       def self.from_h( polygon_hash ) 
         self.new( :points => polygon_hash[ POINTS_KEY ].map{ |point_hash| Point.from_h( point_hash )})
       end
