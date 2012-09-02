e6246618be655cfada84f47bf0d656c0e8ffa452
Farrel Lifson <farrel.lifson@aimred.com>
2012-09-02 18:02:05 +0200

    Moved Resource.from_xml_element to Base namespace

--- lib/rcap/base/resource.rb
+++ lib/rcap/base/resource.rb
@@ -55,6 +55,16 @@ module RCAP
         xml_element
       end
       
+      # @param [REXML::Element] resource_xml_element
+      # @return [Resource]
+      def self.from_xml_element( resource_xml_element ) 
+        resource = self.new( :resource_desc => RCAP.xpath_text( resource_xml_element, RESOURCE_DESC_XPATH, Alert::XMLNS ),
+                             :uri           => RCAP.xpath_text( resource_xml_element, URI_XPATH, Alert::XMLNS ),
+                             :mime_type     => RCAP.xpath_text( resource_xml_element, MIME_TYPE_XPATH, Alert::XMLNS ),
+                             :size          => RCAP.xpath_text( resource_xml_element, SIZE_XPATH, Alert::XMLNS ).to_i,
+                             :digest        => RCAP.xpath_text( resource_xml_element, DIGEST_XPATH, Alert::XMLNS ))
+      end
+      
       # Calculates the SHA-1 hash and size of the contents of {#deref_uri}.
       # Returns an array containing the size (in bytes) and SHA-1 hash if
       # {#deref_uri} is present otherwise returns nil.
